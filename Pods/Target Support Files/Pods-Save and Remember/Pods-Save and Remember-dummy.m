db53ef8cc2e759cd4880ea493af299562b709935
Islam Hemida <isigamer@hotmail.com>
2020-08-31 23:35:50 +0200

    test

---
+++ Pods/Target Support Files/Pods-Save and Remember/Pods-Save and Remember-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Save_and_Remember : NSObject
+@end
+@implementation PodsDummy_Pods_Save_and_Remember
+@end
--- Save and Remember/ViewController.swift
+++ Save and Remember/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
+    
+    var username = ""
+    var password = ""
+    var ref: DatabaseReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        ref = Database.database().reference()
+        
+        ref.child("accounts").observeSingleEvent(of: .value) { (snapshot) in
+            print(snapshot)
+        }
     }
 
+    
+    @IBOutlet weak var Username: UITextField!
+    
+    @IBOutlet weak var Password: UITextField!
+    
+    @IBAction func didTapButton(_ sender: Any){
+        username = Username.text!
+        password = Password.text!
+        print(username)
+        print(password)
+        
+        self.ref.child("users").child("useruid").setValue(["username": username])
+        self.ref.child("users").child("useruid").setValue(["password": password])
 
+
+     }
+    @IBOutlet weak var Submit: UIButton!
+    
+    
 }
 
